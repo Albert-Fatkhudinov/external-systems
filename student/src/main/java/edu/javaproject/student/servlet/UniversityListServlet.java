70eea1aa30ce57581c7c276a90ca48a7ee765069
Albert-Fatkhudinov <fasberg2@gmail.com>
2020-05-08 19:46:14 +0300

    update web.xml

--- student/src/main/java/edu/javaproject/student/servlet/UniversityListServlet.java
+++ student/src/main/java/edu/javaproject/student/servlet/UniversityListServlet.java
@@ -1,4 +1,18 @@
 package edu.javaproject.student.servlet;
 
-public class UniversityListServlet{
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UniversityListServlet", urlPatterns = {"/universityList"})
+public class UniversityListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/universityList.jsp").forward(req, resp);
+    }
 }
